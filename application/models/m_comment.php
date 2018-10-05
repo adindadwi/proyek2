d393ed336ffd389dff3d035f6aa876a602d597f6
HidayatiMazmi <HidayatiMazmi@users.noreply.github.com>
2018-10-05 08:59:49 +0700

    perbaikan komen

--- application/models/m_comment.php
+++ application/models/m_comment.php
@@ -8,12 +8,12 @@ class M_comment extends CI_Model
   
         public function addComment($data)
         {
-            $this->db->insert('comment',$data);
+            $this->db->insert('tb_komen',$data);
         }
          function posts($num,$start)
 		{
 			$this->db->select();
-			$this->db->from('comment');
+			$this->db->from('tb_komen');
 			$this->db->order_by('time','DESC');
 			$this->db->limit($num,$start);
 			$query = $this->db->get();
@@ -22,14 +22,14 @@ class M_comment extends CI_Model
 		function get_com($num,$start)
 		{	
 			$this->db->select();
-			$this->db->from('comment');
+			$this->db->from('tb_komen');
 			$this->db->limit($num,$start);
 			$query = $this->db->get();
 			return $query->result_array();
 		}
 		function get_com_count()
 	{
-		$this->db->select()->from('comment');
+		$this->db->select()->from('tb_komen');
 		$query = $this->db->get();
 		return $query->num_rows();
 	}
